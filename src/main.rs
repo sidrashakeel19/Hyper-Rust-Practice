fb3bd0899e630e1d131ef521593bd8abed2daea0
home <sidrashakeel19@hotmail.com>
2020-08-03 23:33:18 +0500

    example 1

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+//Example 1 : Getting Started with Server
+
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+#[tokio::main]
+async fn main() {
+    //we'll bind to 127.0.0.1:3000
+    let addr = SocketAddr::from(([127,0,0,1], 3000));
+
+    //A 'Service' is needed for every connection, sp this
+    //creates one from our "hello_world" function
+
+    let server = Server::bind(&addr).serve(make_service_fn(|_conn| async {
+        //service_fn converts our function into a 'Service'
+        Ok::<_, Infallible>(service_fn(hello_world))
+    }));
+
+    //Run this server for...forever!
+
+    if let Err(e) = server.await {
+        eprintln!("Server error: {}", e);
+    }
+}
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, Sidra! - From HYper".into()))
+}
